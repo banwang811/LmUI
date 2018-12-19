c41bb86927f9186b67ef363fbe88daea288b2235
banwang811 <759919341@qq.com>
2018-12-19 11:19:29 +0800

    init

---
+++ LMUI/Classes/YWTest.h
@@ -0,0 +1,17 @@
+//
+//  YWTest.h
+//  LMUI_Example
+//
+//  Created by banwang on 2018/12/19.
+//  Copyright © 2018 759919341@qq.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface YWTest : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ LMUI/Classes/YWTest.m
@@ -0,0 +1,13 @@
+//
+//  YWTest.m
+//  LMUI_Example
+//
+//  Created by banwang on 2018/12/19.
+//  Copyright © 2018 759919341@qq.com. All rights reserved.
+//
+
+#import "YWTest.h"
+
+@implementation YWTest
+
+@end
